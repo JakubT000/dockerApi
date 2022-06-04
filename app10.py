81d2455b78788722593dfc472a5e7944309fae87
JakubT000 <100494434+JakubT000@users.noreply.github.com>
2022-06-04 18:07:47 +0200

    Add files via upload
    zmiany

---
+++ app10.py
@@ -0,0 +1,28 @@
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1.0/predict", methods=['GET'])
+def make_prediction():
+    x1 = request.args.get("x1", 0, type=float)
+    x2 = request.args.get("x2", 0, type=float)
+    x3 = request.args.get("x3", 0, type=float)
+    x4 = request.args.get("x4", 0, type=float)
+    
+    arr = np.array([[x1,x2,x3,x4]])
+    pickled_model = pickle.load(open('perc.pkl', 'rb'))
+    result = pickled_model.predict(arr) 
+    
+    
+    return jsonify(result)
+if __name__ == '__main__':
+    app.run()
